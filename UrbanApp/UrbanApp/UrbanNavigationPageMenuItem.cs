852b489775cc9c21d7be28af2a4f778df983c8cb
Jesper Engdahl <p3pp33ng@gmail.com>
2018-01-09 11:28:21 +0100

    Created a navigation page to help navigate

---
+++ UrbanApp/UrbanApp/UrbanNavigationPageMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanApp
+{
+
+    public class UrbanNavigationPageMenuItem
+    {
+        public UrbanNavigationPageMenuItem()
+        {
+            TargetType = typeof(UrbanNavigationPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
